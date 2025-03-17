713be1062c66cd4379c319bcb74c9167058403cb
anticyc <binglinlau@gmail.com>
2025-03-17 10:26:28 +0800

    Update models: fixed migration errors.

--- ims/models.py
+++ ims/models.py
@@ -26,86 +26,86 @@ class User(models.Model):
             "deleted": self.deleted
         }
 
-    # def conversations(self): # 返回conversation列表
-    #     convs = Conversation.objects.filter(members=self)
-    #     return {
-    #         "email": self.email, 
-    #         "name": self.name, 
-    #         "conversations": [ return_field(conv.serialize(), ["id", "boardName", "userName", "createdAt"])
-    #                    for conv in convs ]
-    #     }
+    def conversations(self): # 返回conversation列表
+        convs = Conversation.objects.filter(members=self)
+        return {
+            "email": self.email, 
+            "name": self.name, 
+            "conversations": [ return_field(conv.serialize(), ["id", "content", "sender", "conversation", "created_time"])
+                       for conv in convs ]
+        }
     
     def __str__(self) -> str:
         return self.email
 
-# class Conversation(models.Model):
-#     # types: 0-private, 1-group
-#     type = models.BooleanField(default=False)
-#     id = models.BigAutoField(primary_key=True)
-#     members = models.ManyToManyField(User)
-#     created_time = models.FloatField(default=utils_time.get_timestamp)
-#     avatar = models.ImageField(upload_to='avatar/conversation/')
-#     creator = models.ForeignKey(User, on_delete=models.CASCADE)
-#     managers = models.ManyToManyField(User)
-#     last_message_id = models.IntegerField(default=-1)
+class Conversation(models.Model):
+    # types: 0-private, 1-group
+    type = models.BooleanField(default=False)
+    id = models.BigAutoField(primary_key=True)
+    members = models.ManyToManyField(User)
+    created_time = models.FloatField(default=utils_time.get_timestamp)
+    avatar = models.ImageField(upload_to='avatar/conversation/')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    managers = models.ManyToManyField(User, related_name="managers")
+    last_message_id = models.IntegerField(default=-1)
     
-#     class Meta:
-#         indexes = [models.Index(fields=["id"])]
+    class Meta:
+        indexes = [models.Index(fields=["id"])]
 
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "type": self.type,
-#             "avatar": self.avatar.url,
-#             "creator": self.creator.email,
-#             "last_message_id": self.last_message_id,
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "avatar": self.avatar.url,
+            "creator": self.creator.email,
+            "last_message_id": self.last_message_id,
+        }
 
-#     def __str__(self) -> str:
-#         return f"conversation {self.id}"
+    def __str__(self) -> str:
+        return f"conversation {self.id}"
 
-# class Message(models.Model):
-#     id = models.BigAutoField(primary_key=True)
-#     content = models.TextField() # 是否加密传输
-#     sender = models.ForeignKey(User, on_delete=models.CASCADE)
-#     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
-#     time = models.FloatField(default=utils_time.get_timestamp)
+class Message(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    content = models.TextField() # 是否加密传输
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    time = models.FloatField(default=utils_time.get_timestamp)
 
-#     class Meta:
-#         indexes = [models.Index(fields=["id", "time"])]
+    class Meta:
+        indexes = [models.Index(fields=["id", "time"])]
 
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "content": self.content,
-#             "sender": self.sender.email,
-#             "conversation": self.conversation.id,
-#             "created_time": self.time
-#         }
-#     def __str__(self) -> str:
-#         return f"message {self.id}"
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "sender": self.sender.email,
+            "conversation": self.conversation.id,
+            "created_time": self.time
+        }
+    def __str__(self) -> str:
+        return f"message {self.id}"
 
-# class Request(models.Model):
-#     id = models.BigAutoField(primary_key=True)
-#     sender = models.ForeignKey(User, on_delete=models.CASCADE)
-#     receiver = models.ForeignKey(User, on_delete=models.CASCADE)
-#     time = models.FloatField(default=utils_time.get_timestamp)
-#     status = models.IntegerField(default=False) # 0-waiting, 1-rejected, 2-accepted
+class Request(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="request_sender")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="request_receiver")
+    time = models.FloatField(default=utils_time.get_timestamp)
+    status = models.IntegerField(default=False) # 0-waiting, 1-rejected, 2-accepted
 
-#     class Meta:
-#         indexes = [models.Index(fields=["id"])]
-#     def __str__(self) -> str:
-#         return f"request from {self.sender.email} to {self.receiver.email}"
+    class Meta:
+        indexes = [models.Index(fields=["id"])]
+    def __str__(self) -> str:
+        return f"request from {self.sender.email} to {self.receiver.email}"
 
-# class Invitation(models.Model):
-#     id = models.BigAutoField(primary_key=True)
-#     sender = models.ForeignKey(User, on_delete=models.CASCADE)
-#     receiver = models.ForeignKey(User, on_delete=models.CASCADE)
-#     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
-#     time = models.FloatField(default=utils_time.get_timestamp)
-#     status = models.IntegerField(default=False) # 0-waiting, 1-rejected, 2-accepted
+class Invitation(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="invitation_sender")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="invitation_receiver")
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    time = models.FloatField(default=utils_time.get_timestamp)
+    status = models.IntegerField(default=False) # 0-waiting, 1-rejected, 2-accepted
 
-#     class Meta:
-#         indexes = [models.Index(fields=["id"])]
-#     def __str__(self) -> str:
-#         return f"invitation from {self.sender.email} to {self.receiver.email} for conversation {self.conversation.id}"
+    class Meta:
+        indexes = [models.Index(fields=["id"])]
+    def __str__(self) -> str:
+        return f"invitation from {self.sender.email} to {self.receiver.email} for conversation {self.conversation.id}"
