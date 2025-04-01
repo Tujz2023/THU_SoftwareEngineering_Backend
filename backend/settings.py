7e2dc18d11f9289ee6d896e6e7e436e2889f73f5
zhanghw <hanwenzhang23@gamil.com>
2025-04-01 11:58:24 +0800

    fix:corsheaders

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'daphne',
     'ims',
-    'corsheaders'
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
