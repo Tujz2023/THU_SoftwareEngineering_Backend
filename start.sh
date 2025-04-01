ce1f86a050336e7468878cabec31844ebca70b85
zhanghw <hanwenzhang23@gamil.com>
2025-04-01 12:17:10 +0800

    feat:change to asgi

--- start.sh
+++ start.sh
@@ -2,14 +2,16 @@
 python3 manage.py makemigrations ims
 python3 manage.py migrate
 
-# TODO Start: [Student] Run with uWSGI instead
-# python3 manage.py runserver 80
-uwsgi --module=backend.wsgi:application \
-    --env DJANGO_SETTINGS_MODULE=backend.settings \
-    --master \
-    --http=0.0.0.0:80 \
-    --processes=5 \
-    --harakiri=20 \
-    --max-requests=5000 \
-    --vacuum
-# TODO End: [Student] Run with uWSGI instead
+# # TODO Start: [Student] Run with uWSGI instead
+# # python3 manage.py runserver 80
+# uwsgi --module=backend.wsgi:application \
+#     --env DJANGO_SETTINGS_MODULE=backend.settings \
+#     --master \
+#     --http=0.0.0.0:80 \
+#     --processes=5 \
+#     --harakiri=20 \
+#     --max-requests=5000 \
+#     --vacuum
+# # TODO End: [Student] Run with uWSGI instead
+
+uvicorn backend.asgi:application --host 0.0.0.0 --port 80 --workers 5
