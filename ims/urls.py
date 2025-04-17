182d178988cb86055fcb1e76a83c58b6ea903180
zhanghw23 <hanwenzhang23@gmail.com>
2025-04-17 23:51:39 +0800

    feat:add api for get reply_message

--- ims/urls.py
+++ ims/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path('search_user_detail', views.search_user_detail),
     path('conversations', views.conversation),
     path('conversations/messages', views.message),
+    path('conversations/get_reply', views.get_reply),
     path('conversations/manage/admin', views.conv_manage_admin),
     path('conversations/manage/info', views.conv_manage_info),
     path('conversations/manage/ownership_transfer', views.conv_manage_ownership),
